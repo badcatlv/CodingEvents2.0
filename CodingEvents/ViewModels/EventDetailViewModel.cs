1abc7a6b90920d4a823a77a6efd0d4b6c78b1cc2
Colleen Blake <colleenemilyblake@gmail.com>
2023-11-25 18:09:50 -0600

    created a many to many git status

--- CodingEvents/ViewModels/EventDetailViewModel.cs
+++ CodingEvents/ViewModels/EventDetailViewModel.cs
@@ -11,6 +11,7 @@ namespace CodingEvents.ViewModels
         public string Description { get; set; }
         public string ContactEmail { get; set; }
         public string CategoryName { get; set; }
+        public string TagText { get; set; }
 
         public EventDetailViewModel(Event theEvent)
         {
@@ -19,6 +20,18 @@ namespace CodingEvents.ViewModels
             Description = theEvent.Description;
             ContactEmail = theEvent.ContactEmail;
             CategoryName = theEvent.Category.Name;
+
+            TagText = "";
+            List<Tag> evtTags = theEvent.Tags.ToList();
+
+            for(var i = 0; i < evtTags.Count; i++)
+            {
+                TagText += ("#" + evtTags[i].Name);
+                if (i < evtTags.Count - 1)
+                {
+                    TagText += ", ";
+                }
+            }
         }
     }
 }
